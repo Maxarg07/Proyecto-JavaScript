5968c14a17f0cf68dbc11de121d96d9a827a3598
maxargen07 <maxargen07@gmail.com>
2023-05-17 04:14:37 -0600

    Funcion retirar

--- Cajero/js/transacciones.js
+++ Cajero/js/transacciones.js
@@ -18,13 +18,37 @@ function depositar(){
         localStorage.setItem("saldo",saldoDeposito)
         document.getElementById('saldo').innerHTML = localStorage.getItem("saldo");
 
-        alert("Deposito Realizado con exito")
+        alert("Deposito Realizado con exito.")
     }
 }
 
 
 //funcion de retirar
 function retirar(){
+    if(localStorage.getItem('saldoRetirado')!=null){
+
+    }else{
+        localStorage.setItem("saldoRetirado",0)
+    }
+    let saldo = localStorage.getItem("saldo");
+    let retirado = parseInt(document.getElementById('retiro').value);
+
+    if(retirado > saldo){
+        alert("El saldo es insuficiente.")
+    }else{
+        let retirados = parseInt(localStorage.getItem('saldoRetirado'))+retirado;
+        localStorage.setItem("saldoRetirado",retirados);
+
+        if(retirados > 30000){
+            alert("No se puede retirar mas de $30,000")
+        }else{
+            let saldoRetirado = parseInt(saldo)-retirado;
+            localStorage.setItem("saldo",saldoRetirado)
+
+            document.getElementById('saldo').innerHTML = localStorage.getItem("saldo");
+            alert("Retiro realizado con exito.")
+        }
+    }
 
 }
 
