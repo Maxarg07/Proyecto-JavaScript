f51b88170f54fb1fc0a2125fecd372ee28adb74c
maxargen07 <maxargen07@gmail.com>
2023-05-17 01:50:10 -0600

    mensaje de alerta en el loggin

--- Cajero/js/logica.js
+++ Cajero/js/logica.js
@@ -23,13 +23,16 @@ function login(){
     let success = false;
     
     let i = 0;
-    while(!success && (i < user.length)){
+    while(!success && (i < users.length)){
 
         if( (user == users[i].username) && (pass == users[i].password) ){
             success = true;
             window.location.href = "..\\html\\pagina-principal.html";
         }
         i++;
+    }
+    if(!success && (i == users.length)){
+        alert("Credenciales incorrectas");
     }
 }
 
